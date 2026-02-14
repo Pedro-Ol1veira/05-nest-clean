236ab3efb0442ea4a1daba10e3d8dbdd77db27de
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2026-02-14 14:02:56 -0300

    Configurando DB para testes e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'prisma/generated/client';
+import { randomUUID } from 'crypto';
+import { execSync } from 'child_process';
+
+
+const connectionString = process.env.DATABASE_URL;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+
+function generateUniqueDatabaseUrl(schemaId: string) {
+    if(!process.env.DATABASE_URL) throw new Error("Please provide Database url variable");
+    const url = new URL(process.env.DATABASE_URL);
+    url.searchParams.set('schema', schemaId);
+
+    return url.toString();
+}
+
+const schemaId = randomUUID();
+
+beforeAll(async () => {
+    const databaseUrl = generateUniqueDatabaseUrl(schemaId);
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync("npx prisma migrate deploy");
+});
+
+afterAll(async () => {
+    await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`);
+    await prisma.$disconnect();
+})
